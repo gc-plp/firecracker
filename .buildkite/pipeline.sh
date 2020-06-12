6124c9ee6fcc3b3811a30fe46f0a623e772dbbaf
Gabriel Ionescu <gbi@amazon.com>
2020-06-12 18:56:35 +0300

    Add label
    Signed-off-by: Gabriel Ionescu <gbi@amazon.com>

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -1,5 +1,6 @@
 for i in $(find ./tests/integration_tests/* -name "*.py"); do
     echo "- command: ./tools/devtool -y test -- .$i"
+    echo "  label: $i"
     echo "  agents:"
     echo "    \"stack=buildkite\""
     echo ""
