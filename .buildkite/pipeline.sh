e6133f5b75810abd81ec4517d27c64d56e32b609
Gabriel Ionescu <gbi@amazon.com>
2020-06-12 17:54:44 +0300

    update buildkite pipeline script
    Signed-off-by: Gabriel Ionescu <gbi@amazon.com>

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -1,3 +1,5 @@
 for i in $(find ./tests/integration_tests/* -name "*.py"); do
     echo "- command: ./tools/devtool -y test -- .$i"
+    echo "    agents:"
+    echo "    - \"stack=buildkite\""
 done
